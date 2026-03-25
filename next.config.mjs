25ebe0bcd7699fab54b24286f2cccb4eda0bfeee
Nam Le <lov3u1102@gmail.com>
2026-03-25 07:08:47 +0700

    fix vercel ocr bundling

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": [
+        "./node_modules/tesseract.js/src/worker-script/node/**/*",
+        "./node_modules/tesseract.js-core/**/*",
+        "./node_modules/wasm-feature-detect/**/*"
+      ],
+      "/api/fuel-prices": [
+        "./node_modules/tesseract.js/src/worker-script/node/**/*",
+        "./node_modules/tesseract.js-core/**/*",
+        "./node_modules/wasm-feature-detect/**/*"
+      ]
+    }
+  }
 };
 
 export default nextConfig;
